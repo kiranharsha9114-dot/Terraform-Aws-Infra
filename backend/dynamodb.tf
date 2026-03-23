1c9b6a4954521c806b472510a20aa8d9187f9d12
CHINNA KIRAN KUMAR REDDY <kiranharsha9114@gmail.com>
2026-03-23 09:53:45 +0530

    Add DynamoDB table for state locking

---
+++ backend/dynamodb.tf
@@ -0,0 +1,18 @@
+# -----------------------------
+# DynamoDB Table for State Locking (MUST for teams)
+# -----------------------------
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "Dev"
+  }
+}
