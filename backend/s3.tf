6207717409b188d569c5c18537a5a5e21deb7917
CHINNA KIRAN KUMAR REDDY <kiranharsha9114@gmail.com>
2026-03-23 09:52:47 +0530

    Create s3.tf

---
+++ backend/s3.tf
@@ -0,0 +1,71 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# -----------------------------
+# S3 Bucket for Terraform State
+# -----------------------------
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "terraform-aws-infra-s3bucket"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+}
+
+# -----------------------------
+# Ownership Controls 
+# -----------------------------
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+# -----------------------------
+# Block Public Access 
+# -----------------------------
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.ownership
+  ]
+}
+
+# -----------------------------
+# Versioning 
+# ----------------------------
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# -----------------------------
+# Encryption 
+# -----------------------------
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
